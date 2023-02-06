3f0e146ae0f2ebb18fbe9b6c7a4cf9eb175b5243
Cootakahashi <saiko.life2020@gmail.com>
2023-02-07 02:21:08 +0900

    added . all

---
+++ pages/blog/[id].js
@@ -0,0 +1,35 @@
+import { client } from '../../libs/client'
+// SSG
+export const getStaticProps = async (context) => {
+    const id = context.params.id;
+    const data = await client.get({ endpoint: 'noin', contentId: id });
+
+    return {
+        props: {
+            blog: data,
+        },
+    };
+};
+
+export const getStaticPaths = async() => {
+    const data = await client.get({ endpoint: 'noin' })
+
+    const paths = data.contents.map((content) => `/blog/${content.id}`);
+    console.log(paths)
+    return {
+        paths,
+        //fallbackでpathがなければ404を返す
+        fallback: false,
+        
+    } 
+}
+
+export default function BlogId({ blog }) {
+    return (
+        <main>
+            <h1>{ blog.title }</h1>
+            <p>{ blog.publishedAt }</p>
+            <div dangerouslySetInnerHTML={{__html: `${blog.body}`}}></div>
+        </main>
+    )
+}
