eca2d038896851c6b6594d9200a22cff1110958c
Cootakahashi <saiko.life2020@gmail.com>
2023-02-11 22:35:25 +0900

    change path

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,8 @@ import { client } from "../libs/client";
 import { Nav } from "../components/header";
 import { Footer } from "../components/footer";
 import { Event } from "../components/event";
+import Script from "next/script";
+import * as gtag from "../lib/gtag";
 //SSG
 import React, { useState, useEffect } from "react";
 
